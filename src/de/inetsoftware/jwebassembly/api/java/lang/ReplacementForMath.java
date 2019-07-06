605a6545ca7bc6a941215e5c5118bcf2698a6ae1
Volker Berlin <volker.berlin@googlemail.com>
2019-07-06 15:56:57 +0200

    more replacement for Math functions

--- src/de/inetsoftware/jwebassembly/api/java/lang/ReplacementForMath.java
+++ src/de/inetsoftware/jwebassembly/api/java/lang/ReplacementForMath.java
@@ -17,50 +17,147 @@ package de.inetsoftware.jwebassembly.api.java.lang;
 
 import de.inetsoftware.jwebassembly.api.annotation.Import;
 import de.inetsoftware.jwebassembly.api.annotation.Replace;
+import de.inetsoftware.jwebassembly.api.annotation.WasmTextCode;
 
 /**
  * Replacement methods for the class java.lang.Math.
  * 
  * @author Volker Berlin
  */
+@SuppressWarnings( "unused" )
 class ReplacementForMath {
 
     /**
      * Replacement for {@link Math#sin(double)}
      * 
-     * @param a
+     * @param x
      *            an angle, in radians.
      * @return the sine of the argument.
      */
     @Replace( "java/lang/Math.sin(D)D" )
     @Import( module = "Math", name = "sin" )
-    static double sin( double a ) {
+    static double sin( double x ) {
         return 0; // for Java compiler
     }
 
     /**
      * Replacement for {@link Math#cos(double)}
      * 
-     * @param a
+     * @param x
      *            an angle, in radians.
      * @return the cosine of the argument.
      */
     @Replace( "java/lang/Math.cos(D)D" )
     @Import( module = "Math", name = "cos" )
-    static double cos( double a ) {
+    static double cos( double x ) {
         return 0; // for Java compiler
     }
 
     /**
      * Replacement for {@link Math#tan(double)}
      * 
-     * @param a
+     * @param x
      *            an angle, in radians.
      * @return the tangent of the argument.
      */
     @Replace( "java/lang/Math.tan(D)D" )
     @Import( module = "Math", name = "tan" )
-    static double tan( double a ) {
+    static double tan( double x ) {
         return 0; // for Java compiler
     }
+
+    /**
+     * Replacement for {@link Math#asin(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the arc sine of the argument.
+     */
+    @Replace( "java/lang/Math.asin(D)D" )
+    @Import( module = "Math", name = "asin" )
+    static double asin( double x ) {
+        return 0; // for Java compiler
+    }
+
+    /**
+     * Replacement for {@link Math#acos(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the arc cosine of the argument.
+     */
+    @Replace( "java/lang/Math.acos(D)D" )
+    @Import( module = "Math", name = "acos" )
+    static double acos( double x ) {
+        return 0; // for Java compiler
+    }
+
+    /**
+     * Replacement for {@link Math#atan(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the arc tangent of the argument.
+     */
+    @Replace( "java/lang/Math.atan(D)D" )
+    @Import( module = "Math", name = "atan" )
+    static double atan( double x ) {
+        return 0; // for Java compiler
+    }
+
+    /**
+     * Replacement for {@link Math#exp(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the value <i>e</i><sup>x</sup>
+     */
+    @Replace( "java/lang/Math.exp(D)D" )
+    @Import( module = "Math", name = "exp" )
+    static double exp( double x ) {
+        return 0; // for Java compiler
+    }
+
+    /**
+     * Replacement for {@link Math#log(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the value ln x
+     */
+    @Replace( "java/lang/Math.log(D)D" )
+    @Import( module = "Math", name = "log" )
+    static double log( double x ) {
+        return 0; // for Java compiler
+    }
+
+    /**
+     * Replacement for {@link Math#log10(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the base 10 logarithm of x
+     */
+    @Replace( "java/lang/Math.log10(D)D" )
+    @Import( module = "Math", name = "log10" )
+    static double log10( double x ) {
+        return 0; // for Java compiler
+    }
+
+
+    /**
+     * Replacement for {@link Math#sqrt(double)}
+     * 
+     * @param x
+     *            the value.
+     * @return the positive square root of x
+     */
+    @Replace( "java/lang/Math.sqrt(D)D" )
+    @WasmTextCode( "local.get 0 " //
+                    + "f64.sqrt " //
+                    + "return" )
+    static double sqrt( double x ) {
+        return 0; // for Java compiler
+    }
+
 }
