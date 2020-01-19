b3ff47a59cfe0f41105791b9b662397331ada55b
Volker Berlin <volker.berlin@googlemail.com>
2020-01-19 15:28:39 +0100

    Add support for partial classes as overlay.

---
+++ src/de/inetsoftware/jwebassembly/api/annotation/Partial.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Volker Berlin (i-net software)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.inetsoftware.jwebassembly.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Mark a class as an extension/partial of an existing class.
+ * 
+ * @author Volker Berlin
+ *
+ */
+@Retention( RetentionPolicy.CLASS )
+@Target( { ElementType.TYPE } )
+public @interface Partial {
+
+    /**
+     * The full signature of a class that should be extended like "java/lang/String"
+     * 
+     * @return the signature
+     */
+    String value();
+}
